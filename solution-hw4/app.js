9684453b76769fb675056971711020d0def3224f
wndyli <718wendy@gmail.com>
2022-09-29 21:44:23 -0400

    test case

--- solution-hw4/app.js
+++ solution-hw4/app.js
@@ -81,7 +81,7 @@ rollName.innerText = rollType + ' Cinnamon Roll';
 
 // updates cinnamon roll type
 const rollImage = document.querySelector('#leftpic');
-rollImage.src = '../assets/' + rollType + '-cinnamon-roll.jpeg';
+rollImage.src = 'assets/' + rollType + '-cinnamon-roll.jpeg';
 
 // updates cinnamon roll price
 const rollPrice = document.querySelector('#checkoutprice');
--- solution-hw4/rollsData.js
+++ solution-hw4/rollsData.js
@@ -2,27 +2,27 @@
 const rolls = {
     "Original": {
         "basePrice": 2.49,
-        "imageFile": "original-cinnamon-roll.jpg"
+        "imageFile": "original-cinnamon-roll.jpeg"
     },
     "Apple": {
         "basePrice": 3.49,
-        "imageFile": "apple-cinnamon-roll.jpg"
+        "imageFile": "apple-cinnamon-roll.jpeg"
     },
     "Raisin": {
         "basePrice": 2.99,
-        "imageFile": "raisin-cinnamon-roll.jpg"
+        "imageFile": "raisin-cinnamon-roll.jpeg"
     },
     "Walnut": {
         "basePrice": 3.49,
-        "imageFile": "walnut-cinnamon-roll.jpg"
+        "imageFile": "walnut-cinnamon-roll.jpeg"
     },
     "Double-Chocolate": {
         "basePrice": 3.99,
-        "imageFile": "double-chocolate-cinnamon-roll.jpg"
+        "imageFile": "double-chocolate-cinnamon-roll.jpeg"
     },
     "Strawberry": {
         "basePrice": 3.99,
-        "imageFile": "strawberry-cinnamon-roll.jpg"
+        "imageFile": "strawberry-cinnamon-roll.jpeg"
     }    
 };
 
